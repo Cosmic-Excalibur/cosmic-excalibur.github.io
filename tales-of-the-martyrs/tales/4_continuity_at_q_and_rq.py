77bc78262588e20c4d95700851ba53a57bc65b69
tRPDE <142775777+Cosmic-Excalibur@users.noreply.github.com>
2024-07-09 17:29:13 +0800

    New Article

---
+++ tales-of-the-martyrs/tales/4_continuity_at_q_and_rq.py
@@ -0,0 +1,14 @@
+INFO = {
+    'title': r'Continuity at \(\mathbb{Q}\) and \(\mathbb{R}\backslash\mathbb{Q}\)',
+    'tags': ['Mathematics', 'GeneralTopology', 'RealAnalysis'],
+    'time': '2024-07-09 16:35'
+}
+
+def info():
+    return Attrs(INFO)
+    
+data = lambda: {
+    "*.html": {
+        __CONTENT__: open(".".join(path.split(".")[:-1]) + ".data", 'r', encoding = 'utf-8').read()
+    }
+}
