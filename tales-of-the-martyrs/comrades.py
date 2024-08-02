31be4250ff8ca607acda7fc1113f70484808e8ce
tRPDE <142775777+Cosmic-Excalibur@users.noreply.github.com>
2024-08-03 00:39:37 +0800

    ともだち

--- tales-of-the-martyrs/comrades.py
+++ tales-of-the-martyrs/comrades.py
@@ -1,15 +1,16 @@
 entries = {
     'Astrageldon': 'https://cosmic-excalibur.github.io',
     'Astrageldon <comment>(Github)</comment>': 'https://github.com/Cosmic-Excalibur',
-    'PJSK Stickers': 'https://trpde.github.io',
     'Tplus': 'https://www.tpluszz.top',
     '<s>App1e_Tree</s> <comment>(Outdated)</comment>': 'https://www.cnblogs.com/App1eTree',
     'App1e_Tree': 'https://yinwww.wang',
     'LiBr': 'https://nvme0n1p.dev',
+    'LiBr <comment>(Github)</comment>': 'https://github.com/lbr77',
     'CrackTC': 'https://sora.zip',
     'CrackTC <comment>(Github)</comment>': 'https://github.com/CrackTC',
     'emmm <comment>(Github)</comment>': 'https://github.com/H4ckF0rFun',
-    'Spirit Team': 'https://jluctf.top'
+    'Spirit Team': 'https://jluctf.top',
+    'PJSK Stickers': 'https://trpde.github.io'
 }
 
 def parse_entry(entries):
@@ -25,7 +26,7 @@ data = lambda: {
                 r"""<hr>
                 <div style="margin-left:2em">
                     <h2>{0}</h2>
-                    <span style="color:gray">Link🔗: </span><a underlined href="{1}">{1}</a>
+                    <span style="color:gray">Link: </span><a underlined href="{1}">{1}</a>
                 </div>
                 """.format(*entry) for entry in parse_entry(entries)
             ) + "\n<br><br><hr><br><br>"
