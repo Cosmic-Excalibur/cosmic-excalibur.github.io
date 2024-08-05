ebdcfaa2d6b2408b0aa4d7e946fb19726f77b105
tRPDE <142775777+Cosmic-Excalibur@users.noreply.github.com>
2024-08-05 20:04:11 +0800

    ともだち

--- tales-of-the-martyrs/comrades.py
+++ tales-of-the-martyrs/comrades.py
@@ -1,21 +1,21 @@
 entries = {
-    'Astrageldon': 'https://cosmic-excalibur.github.io',
-    'Astrageldon <comment>(Github)</comment>': 'https://github.com/Cosmic-Excalibur',
-    'Tplus': 'https://www.tpluszz.top',
-    '<s>App1e_Tree</s> <comment>(Outdated)</comment>': 'https://www.cnblogs.com/App1eTree',
-    'App1e_Tree': 'https://yinwww.wang',
-    'LiBr': 'https://nvme0n1p.dev',
-    'LiBr <comment>(Github)</comment>': 'https://github.com/lbr77',
-    'CrackTC': 'https://sora.zip',
-    'CrackTC <comment>(Github)</comment>': 'https://github.com/CrackTC',
-    'emmm <comment>(Github)</comment>': 'https://github.com/H4ckF0rFun',
-    'Spirit Team': 'https://jluctf.top',
-    'PJSK Stickers': 'https://trpde.github.io'
+    'Astrageldon': ['https://cosmic-excalibur.github.io'],
+    'Astrageldon <comment>(Github)</comment>': ['https://github.com/Cosmic-Excalibur'],
+    'Tplus': ['https://www.tpluszz.top'],
+    '<s>App1e_Tree</s> <comment>(Outdated)</comment>': ['https://www.cnblogs.com/App1eTree'],
+    'App1e_Tree': ['https://yinwww.wang'],
+    'LiBr': ['https://nvme0n1p.dev'],
+    'LiBr <comment>(Github)</comment>': ['https://github.com/lbr77'],
+    'CrackTC': ['https://sora.zip'],
+    'CrackTC <comment>(Github)</comment>': ['https://github.com/CrackTC'],
+    'emmm <comment>(Github)</comment>': ['https://github.com/H4ckF0rFun'],
+    'Spirit Team': ['https://jluctf.top', 'https://spirit.college'],
+    'PJSK Stickers': ['https://trpde.github.io']
 }
 
 def parse_entry(entries):
     return [
-        entry for entry in list(entries.items())
+        [entry[0], "<br>".join("""{0}<a underlined href="{1}">{1}</a>""".format("""<span style="color:gray">Link: </span>""" if i == 0 else """<span style="color:gray">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>""", link) for i, link in enumerate(entry[1]))] for entry in list(entries.items())
     ]
 
 data = lambda: {
@@ -26,7 +26,7 @@ data = lambda: {
                 r"""<hr>
                 <div style="margin-left:2em">
                     <h2>{0}</h2>
-                    <span style="color:gray">Link: </span><a underlined href="{1}">{1}</a>
+                    {1}
                 </div>
                 """.format(*entry) for entry in parse_entry(entries)
             ) + "\n<br><br><hr><br><br>"
